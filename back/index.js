31a3b7e791ca84d9f1b39a490df5f20975704b62
Roland HUON <huonroland91@gmail.com>
2024-02-25 12:26:29 +0100

    🚧 Add cors middleware and update .gitignore

--- back/index.js
+++ back/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import router from "./route.js";
+import cors from "cors";
 
 const app = express();
 app
+    .use(cors())
     .use(router)
     
 const port = 3000;
