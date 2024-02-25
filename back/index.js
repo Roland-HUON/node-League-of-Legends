2ad6c3904ea0ef1fb98e2275b9096c566561b586
Roland HUON <huonroland91@gmail.com>
2024-02-25 15:04:01 +0100

    Add body-parser middleware for parsing JSON and URL-encoded data

--- back/index.js
+++ back/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import router from "./route.js";
 import cors from "cors";
+import bodyParser from "body-parser";
 
 const app = express();
 app
     .use(cors())
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({extended: true}))
     .use(router)
     
 const port = 3000;
